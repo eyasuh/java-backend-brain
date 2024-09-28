f432beb792264b0375c803c4aea2c1e437ee2313
eyasuh <eyha12@gmail.com>
2024-09-28 18:14:23 -0400

    InstanceOF operator challenge

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/InstanceOf.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/InstanceOf.java
@@ -13,5 +13,16 @@ public class InstanceOf {
 
         // Challenge: Use instanceof to determine the type of each object in the array
 
+        for (Object object : objects) {
+            String instance ;
+            if (object instanceof Integer) instance = "Integer";
+            else if (object instanceof String) instance = "String";
+            else if (object instanceof Boolean) instance = "Boolean";
+            else if (object instanceof Double) instance = "Double";
+            else instance = "primitive";
+            System.out.println(instance);
+
+        }
+
     }
 }
