5421ea6eafe8cf6c5f0771f5c2db40ed3c160739
eyasuh <eyha12@gmail.com>
2024-09-28 17:52:14 -0400

    Wrapper classes challenge

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/PrimitiveAndWrapperClasses.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/PrimitiveAndWrapperClasses.java
@@ -3,14 +3,16 @@ package io.javabrains.javabasics;
 public class PrimitiveAndWrapperClasses {
     public static void main(String[] args) {
         // Step 1: Declare primitive variables
-
+        int intNUm = 5;
+        double doubleNum = 6;
         // Step 2: Convert primitive variables to wrapper objects
-
+        Integer integerNum = Integer.valueOf(intNUm);
+        Double doubleNum2 = doubleNum;
         // Step 3: Print the values of the wrapper objects
-
+        System.out.println(integerNum.toString() + " " + doubleNum2);
         // Step 4: Convert wrapper objects back to primitive variables
-
+        int backToIntNUm = integerNum.intValue();
         // Step 5: Print the values of the primitive variables
-
+        System.out.println(backToIntNUm);
     }
 }
